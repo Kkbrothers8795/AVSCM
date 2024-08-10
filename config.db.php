eb0a6b2137108952b59eedf2ed0c16a892d66bb8
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-08-10 11:14:17 +0530

    Create config.db.php

---
+++ config.db.php
@@ -0,0 +1,8 @@
+<?php
+defined('_VALID') or die('Restricted Access!');
+$config['db_type'] = 'mysqli';
+$config['db_host'] = 'localhost';
+$config['db_user'] = 'admin_avs';
+$config['db_pass'] = 'kkraju00@';
+$config['db_name'] = 'admin_avs';
+?>
